d4545b4846fbdec2dff610a3167fcc68afe0ac65
Cody Rickman <cody@creativemindstudios.com>
2019-01-11 17:39:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
     if (@discount == 0)
       puts "There is no discount to apply."
     else
-      @total = @total - (@total * (discount/100))
+      @total = @total - (@total * (@discount/100))
       puts "After the discount, the total comes to $#{@total}."
     end
   end
